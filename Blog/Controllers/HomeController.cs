c9a8a1f54408abb3750f17c19478c14639bea1ab
Chunwei Yu <chunwyu@microsoft.com>
2021-01-20 23:50:30 +0800

    init

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("api/ping")]
+        public IActionResult Index()
+        {
+            return Ok("Let Refresh us everyday and go on!!!");
+        }
+    }
+}
